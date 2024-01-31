75c94b39611a8c64e936fe5ba8f7159d969d0b0d
enasr <arenas.eugene@gmail.com>
2024-01-31 09:49:17 +0800

    first commit

---
+++ src/main/java/com/ea/junkins/JenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.ea.junkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+		
+		System.out.println("Hello World");
+	}
+
+}
